0caef96b395ab31c01206c09df444d46fe6f53c1
Adam Olson <adam.lee.olson@outlook.com>
2024-02-18 15:45:49 -0600

    linked together about page, decided on just using separate pages for simplicity right now.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,22 @@
+// app.jsx
+
 import { useEffect, useState } from 'react';
 import MyAppContext from '../contexts/MyAppContext';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { auth } from '../firebase'; // Import Firebase auth instance
-import { firebaseConfig } from '../firebase'; // Import Firebase configuration
+import { firebaseConfig } from '../firebase';
 
 function MyApp({ Component, pageProps }) {
   const [authInstance, setAuthInstance] = useState(null);
 
   useEffect(() => {
-    // Initialize Firebase app and authentication
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
     setAuthInstance(auth);
+
+    return () => {
+      // Cleanup
+    };
   }, []);
 
   return (
---
+++ pages/about.jsx
@@ -0,0 +1,16 @@
+// pages/about.jsx
+
+import React from 'react';
+import Layout from '../components/Layout';
+const AboutPage = () => {
+  return (
+   <Layout>
+   <div>
+      <h1>About Page</h1>
+      {/* Add your about content here */}
+    </div>
+    </Layout>
+  );
+};
+
+export default AboutPage;
